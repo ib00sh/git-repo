69101b50632161be3d3e65f9d728bcba553da381
ib00sh <wowmaxwow98@gmail.com>
2018-08-05 17:33:15 +0300

    проставлены точки с запятой

--- src/Hello.java
+++ src/Hello.java
@@ -1,11 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!;");
 <<<<<<< HEAD
 
 =======
-        System.out.println("Bye");
-        System.out.println("this is the end");
+        System.out.println("Bye;");
+        System.out.println("this is the end;");
 >>>>>>> master
     }
 }
